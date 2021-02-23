e6685fc94ded5fa7968548dd2f85001c6f4c41f1
mohadese <mohi44pakdin@gmail.com>
2021-02-23 07:33:26 -0800

    create user model - 1399/12/5 by Mohadese

---
+++ user/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from apps.user.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+    list_display_links = ['username']
---
+++ user/models/user.py
@@ -0,0 +1,19 @@
+from django.db import models
+from user.validators import minimum_age
+from django_extensions.db.fields import AutoSlugField
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=200, unique=True)
+    password = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from=['username'])
+    username = models.CharField(primary_key=True,max_length=100, unique=True)
+    birthday = models.DateField(validators=[minimum_age])
+
+    # image for profile
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
