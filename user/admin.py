edbad578ca4b0b1b75d842676eeca350a69790aa
mohadese <mohi44pakdin@gmail.com>
2021-02-25 12:25:52 -0800

    change views and templates 1399/12/7 - by Mohadese

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,19 @@
+# from djaa_list_filter.admin import AjaxAutocompleteListFilterModelAdmin
 from django.contrib import admin
 
 from apps.user.models import User
 
 
+# @admin.register(User)
+# class PersonAdmin(AjaxAutocompleteListFilterModelAdmin):
+#     list_display = ['username', 'email']
+#     autocomplete_list_filter = ('username')
+#
+#     def show_tags(self, obj):
+#         return ' , '.join(obj.tags.values_list('name', flat=True))
+
+#
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class PersonAdmin(admin.ModelAdmin):
     list_display = ['username', 'email']
     list_display_links = ['username']
