d747ff3ad0ad97ea5e8bff8634d8ad0c260edff2
mohadese <mohi44pakdin@gmail.com>
2021-03-06 10:31:15 -0800

    change on_delete in comment_model 99/12/16

--- payamgram_auth/apps/post/models/comment.py
+++ payamgram_auth/apps/post/models/comment.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Comment(models.Model):
     context = models.CharField(max_length=200, blank=True)
-    post = models.ForeignKey(Post, on_delete=models.PROTECT)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE())
     user = models.ForeignKey(User, on_delete=models.PROTECT)
 
     def __str__(self):
